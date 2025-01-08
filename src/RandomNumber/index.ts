15dc1241632f3020b17aced75531f12874263161
N-eeraj <neeraj7rajeena@gmail.com>
2025-01-08 13:53:45 +0530

    Basic float Array method

--- src/RandomNumber/index.ts
+++ src/RandomNumber/index.ts
@@ -7,6 +7,10 @@ interface FloatArgs extends IntArgs {
   precision?: number
 }
 
+interface FloatArrayArgs extends FloatArgs {
+  length?: number
+}
+
 export default class RandomNumber {
   static float({ min, max, precision }: FloatArgs = {}) {
     // args validation
@@ -53,5 +57,27 @@ export default class RandomNumber {
       max: Math.ceil(max ?? 100),
       precision: 0,
     })
+  }
+
+  static floatArray({ min, max, precision, length }: FloatArrayArgs = {}) {
+    // args validation
+    if (min !== undefined && typeof min !== "number") {
+      throw new Error(`min must be a number, received ${min} of type ${typeof min}`)
+    }
+    if (max !== undefined && typeof max !== "number") {
+      throw new Error(`max must be a number, received ${max} of type ${typeof max}`)
+    }
+    if (min !== undefined && max !== undefined && min >= max) {
+      throw new Error(`min should be lesser than max, received min: ${min} & max: ${max}`)
+    }
+    if (precision !== undefined && typeof precision !== "number") {
+      throw new Error(`precision should be a number, received ${precision} of type ${typeof precision}`)
+    }
+    if (length !== undefined && typeof length !== "number") {
+      throw new Error(`length should be a number, received ${length} of type ${typeof length}`)
+    }
+
+    return Array.from({ length: length ?? 1 })
+      .map(() => this.float({ min, max, precision }))
   }
 }
