38a6aaab35c0b6382aa41cc343df7ba5cefaa661
N-eeraj <neeraj7rajeena@gmail.com>
2025-01-17 09:45:59 +0530

    Added basic arg validation for RandomList.choice() & its test cases

--- src/RandomList/index.ts
+++ src/RandomList/index.ts
@@ -1,7 +1,23 @@
 import RandomNumber from "../RandomNumber"
+import {
+  checkIsOfType,
+  checkMinValue,
+} from "../utils/argsValidations"
 
 export default class RandomList {
-  static choice<ListItem>(list: Array<ListItem>): ListItem {
-    return list[RandomNumber.int({ max: list.length - 1 })]
+  static choice<ListItem>(list: Array<ListItem>): ListItem | null {
+    checkIsOfType("list", "array", list)
+
+    try {
+      checkMinValue("list length", list.length, 1)
+    } catch {
+      console.warn("Warning: given empty list, returning null.")
+      return null
+    }
+
+    return list[RandomNumber.int({
+      min: 0,
+      max: list.length - 1,
+    })]
   }
 }
--- src/utils/argsValidations.ts
+++ src/utils/argsValidations.ts
@@ -1,8 +1,17 @@
-export function checkIsOfType(key: string, expectedType: string, arg?: unknown) {
-  if (arg !== undefined && typeof arg !== expectedType) {
-    throw new Error(`${key} must be a expectedType, received ${arg} of type ${typeof arg}`)
-  } else if (expectedType === "number" && arg === Infinity) {
+export function checkIsOfType(key: string, expectedType: string, arg: unknown) {
+  if (expectedType === "array" && Array.isArray(arg)) {
+    return
+  }
+  if (typeof arg !== expectedType) {
+    throw new Error(`${key} must be a ${expectedType}, received ${arg} of type ${typeof arg}`)
+  } else if (expectedType === "number" && (arg === Infinity || arg === -Infinity)) {
     throw new Error(`Expected ${key} to be finite number received Infinity`)
+  }
+}
+
+export function checkOptionalIsOfType(key: string, expectedType: string, arg?: unknown) {
+  if (arg !== undefined) {
+    checkIsOfType(key, expectedType, arg)
   }
 }
 
