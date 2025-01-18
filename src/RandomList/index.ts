49a2d57edab6e312df14d22dea6a3d04c6c65488
N-eeraj <neeraj7rajeena@gmail.com>
2025-01-18 18:56:21 +0530

    Added list shuffle functionality

--- src/RandomList/index.ts
+++ src/RandomList/index.ts
@@ -7,6 +7,7 @@ import {
 
 export default class RandomList {
   static choice<ListItem>(list: Array<ListItem>): ListItem | null {
+    // args validation
     checkIsOfType("list", "array", list)
 
     try {
@@ -23,24 +24,41 @@ export default class RandomList {
   }
 
   static shuffle(list: unknown[], mixCount?: number) {
+    // args validation
     checkIsOfType("list", "array", list)
     checkOptionalIsOfType("mixCount", "number", mixCount)
 
     try {
       checkMinValue("mixCount", mixCount, 1)
     } catch (error) {
+      // handle un-shuffled 
       if (mixCount === 0) {
-        console.warn("Warning: received mix count 0, returning un-shuffled list")
+        console.warn("Warning: received mix count 0, returning un-shuffled list.")
         return list
       }
       throw error
     }
 
+    // check and handle un-shuffled 
     try {
-      checkMinValue("list length", list.length, 1)
+      checkMinValue("list length", list.length, 2)
     } catch {
-      console.warn("Warning: given empty list, returning null.")
-      return []
+      console.warn(`Warning: cannot shuffle list ${list}, list too small, returning un-shuffled list.`)
+      return list
+    }
+
+    // handle default mix count
+    if (!mixCount) {
+      mixCount = Math.floor(list.length * Math.log2(list.length))
+    }
+
+    for (let i = 0; i < mixCount; i++) {
+      const index1 = RandomNumber.int({ max: list.length - 1 })
+      let index2 = index1
+      while (index1 === index2) {
+        index2 = RandomNumber.int({ max: list.length - 1 })
+      }
+      [list[index1], list[index2]] = [list[index2], list[index1]]
     }
 
     return list
